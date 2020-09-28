a76c086cbb9c39c6c95cda40fba405eba0d4b24e
398850094@qq.com <398850094@qq.com>
2020-09-28 11:52:54 +0800

    添加事务处理源判断

--- src/main/java/com/fast/dao/transaction/FastTransactionAspect.java
+++ src/main/java/com/fast/dao/transaction/FastTransactionAspect.java
@@ -1,8 +1,14 @@
 package com.fast.dao.transaction;
 
 
+import cn.hutool.core.util.StrUtil;
+import com.fast.dao.jdbc.TransactionInfo;
+import io.netty.util.concurrent.FastThreadLocal;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
+
+import java.util.Map;
 
 /**
  * 事务拦截器
@@ -13,6 +19,9 @@ import org.springframework.stereotype.Component;
 @Component
 public class FastTransactionAspect {
 
+    private static final FastThreadLocal<String> transactionMethodNameThreadLocal = new FastThreadLocal<>();
+
+
     @Pointcut("@annotation(com.fast.dao.transaction.FastTransactionAuto)")
     public void pointCut() {
     }
@@ -22,7 +31,13 @@ public class FastTransactionAspect {
      * 方法执行前,开启事务
      */
     @Before("pointCut()")
-    public void autoOpenTransactionBefore() {
+    public void autoOpenTransactionBefore(JoinPoint point) {
+        String threadLocalMethodName = transactionMethodNameThreadLocal.get();
+        if (threadLocalMethodName != null) {
+            return;
+        }
+        threadLocalMethodName = point.getSignature().getName();
+        transactionMethodNameThreadLocal.set(threadLocalMethodName);
         FastTransaction.open();
     }
 
@@ -30,16 +45,22 @@ public class FastTransactionAspect {
      * 方法执行后,提交事务
      */
     @After("pointCut()")
-    public void autoCommitTransactionAfter() {
-        FastTransaction.commit();
+    public void autoCommitTransactionAfter(JoinPoint point) {
+        String threadLocalMethodName = transactionMethodNameThreadLocal.get();
+        if (StrUtil.equals(point.getSignature().getName(), threadLocalMethodName)) {
+            FastTransaction.commit();
+        }
     }
 
     /**
      * 方法异常后,回滚事务
      */
     @AfterThrowing("pointCut()")
-    public void autoRollbackTransactionAfterThrowing(){
-        FastTransaction.rollback();
+    public void autoRollbackTransactionAfterThrowing(JoinPoint point) {
+        String threadLocalMethodName = transactionMethodNameThreadLocal.get();
+        if (StrUtil.equals(point.getSignature().getName(), threadLocalMethodName)) {
+            FastTransaction.rollback();
+        }
     }
 
 }
